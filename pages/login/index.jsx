1ad56eec0ebe99a04f9a82824bf61dae85acd57a
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-26 21:29:15 +0200

    fix: create LoginPage component and import it in login/index.jsx

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,16 +1,7 @@
-import { Container, Paper } from '@mui/material'
-import Login from '../../src/components/login/Login'
-import styles from './LoginPage.module.scss'
+import LoginPage from '../../src/components/login/LoginPage.jsx'
 
 export default function index() {
   return (
-    <div className={styles.wrapper}>
-      <Paper elevation={24}>
-      <Container maxWidth="xs" disableGutters>
-        <Login />
-      </Container>
-      </Paper>
-
-    </div>
+    <LoginPage/>
   )
 }
