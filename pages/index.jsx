343e30f13c5094a06f64ac944dd4b703304a93a9
kokiqn <kaloyna.stefanoff@gmail.com>
2021-11-30 23:19:12 +0200

    feat: add Navigation component

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,13 @@ import Time from "../src/components/time/Time";
 import User from "../src/components/user/User";
 import Weather from "../src/components/weather/Weather";
 import Header from "../src/components/header/Header";
+import Navigation from "../src/components/navigation/Navigation";
 
 export default function Index() {
   return(
     <>
       <Header left={<User avatar="../images/avatar.png" name="Jonh Doe" />} right={<><Weather degrees={22} type="cloudy" /> <Time /></>}/>
+      <Navigation/>
     </>
   )
 }
--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -2,16 +2,17 @@ import { Container , Grid , Avatar , Typography } from '@mui/material';
 
 import styles from "./User.module.scss"
 
-export default function User({ avatar = " " , name = " " , size = 90 }) {
+export default function User({ avatar = " " , name = " " , size = 90, headingSize, hasWelcome = false }) {
     return (
         <div className={styles['user-container']}>
-            <Container>
+            <Container disableGutters>
                 <Grid container spacing={2} alignItems="center">
                     <Grid item>
                         <Avatar src={avatar} sx={{ width: size , height: size }}/>
                     </Grid>
                     <Grid item>
-                        <Typography variant="h3">{name}</Typography>
+                        {hasWelcome && <Typography variant="h6">Welcome back,</Typography>}
+                        <Typography variant={headingSize}>{name}</Typography>
                     </Grid>
                 </Grid>
             </Container>
