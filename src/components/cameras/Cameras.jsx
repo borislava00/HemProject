e52d432e42970abfa4a3412174dde544e8d077b7
jivko25 <jivkou@yahoo.com>
2021-12-04 00:20:14 +0200

    change camera component structure

--- src/components/cameras/Cameras.jsx
+++ src/components/cameras/Cameras.jsx
@@ -1,11 +1,39 @@
 import Card from "../card/Card.jsx";
 import { Grid } from "@mui/material";
 import styles from "./Cameras.module.scss";
+import { useState } from "react";
 
 export default function Cameras({ cameras , hasButton }) {
+    const [activeCard, setActiveCard] = useState(0);
+
     return(
         <div className={styles['cameras-wrapper']}>
-            <Grid container spacing={3}>
+            <Grid container>
+                <Grid item xs={8}>
+                    <Card iconUrl={cameras[activeCard].videoUrl} />
+                </Grid>
+                <Grid item xs={4}>
+                    <Grid container className={styles.scroll}>
+                        <div className={styles['items-wrapper']}>
+
+                        {
+                        cameras.map((camera, index) => {
+                            return(
+                                <div className={styles.item} onClick={() => {setActiveCard(index)}}>
+                                    <Card iconUrl={camera.videoUrl} outlined={index == activeCard} 
+                                    className={styles['cameras-views']}/>
+                                </div>
+                                )
+                            })
+                        }
+                        </div>
+                        <Grid item xs={12}>
+                            {hasButton ? <div className={styles.add}><Card iconUrl={'/images/plus.svg'} outlined={true}/></div> : null}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            {/* <Grid container spacing={3}>
                 <Grid item xs={6} container>
                     <Grid item xs container direction="column" spacing={3}>
                         <Grid item xs={2}>
@@ -47,7 +75,7 @@ export default function Cameras({ cameras , hasButton }) {
                         </Grid>
                     </Grid>
                 </Grid>
-            </Grid>
+            </Grid> */}
         </div>
     );
 }
