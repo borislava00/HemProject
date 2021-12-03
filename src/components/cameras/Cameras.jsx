eeb12687043c0252312938780c840223aea9aefb
Borislava <bobiivanova00@gmail.com>
2021-12-03 12:52:23 +0200

    Initial camera comp

---
+++ src/components/cameras/Cameras.jsx
@@ -0,0 +1,53 @@
+import Card from "../card/Card.jsx";
+import { Grid } from "@mui/material";
+import styles from "./Cameras.module.scss";
+
+export default function Cameras({ cameras , hasButton }) {
+    return(
+        <div className={styles['cameras-wrapper']}>
+            <Grid container spacing={3}>
+                <Grid item xs={6} container>
+                    <Grid item xs container direction="column" spacing={3}>
+                        <Grid item xs={2}>
+                            <Card iconUrl={cameras[0].videoUrl} />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <Grid item xs container direction="column" spacing={2}>
+                                <Grid item xs={6}>
+                                    <Grid item xs container direction="row" spacing={1}>
+                                        <Grid item xs={3}>
+                                            <Card iconUrl={cameras[1].videoUrl} />
+                                        </Grid>
+                                        <Grid item xs={3}>
+                                            <Card iconUrl={cameras[2].videoUrl} />  
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Grid item xs container direction="row" spacing={1}>
+                                        <Grid item xs={3}>
+                                            <Card iconUrl={cameras[3].videoUrl} />
+                                        </Grid>
+                                        <Grid item xs={3}>
+                                            <Card iconUrl={cameras[4].videoUrl} /> 
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Grid item xs container direction="row" spacing={1}>
+                                        <Grid item xs={3}>
+                                            <Card iconUrl={cameras[5].videoUrl} />
+                                        </Grid>
+                                        <Grid item xs={3}>
+                                            <Card iconUrl={cameras[0].videoUrl} />
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
