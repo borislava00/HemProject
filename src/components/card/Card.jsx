972e5bbf1020a7ecc0a15330f540a2e0d23fefde
Borislava <bobiivanova00@gmail.com>
2021-12-03 12:59:26 +0200

    add Cameras component and change Card component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,15 +2,24 @@ import { CardContent, CardMedia } from '@mui/material';
 import CardWrapper from '@mui/material/Card';
 import styles from './Card.module.scss';
 
-export default function Card({iconUrl, outlined = false, onClick}){
+export default function Card({iconUrl, outlined = false, onClick }){
+
+    const getComponent = () => {
+        let extention = iconUrl.split('.').pop();
+        if ( extention == 'svg' || extention == 'png' ) {
+            return 'img';
+        } else if ( extention == 'mp4' ) {
+            return 'video';
+        } else {
+            return null;
+        }
+    }
     return(
         <div className={styles.card} onClick={onClick}>
             <CardWrapper variant={outlined ? "outlined" : null}>
                 <CardContent className={styles.content}>
                     {iconUrl ? 
-                    <CardMedia>
-                        <img src={iconUrl} className={styles.image}/>
-                    </CardMedia>    
+                    <CardMedia className={styles.image} src={iconUrl} component={getComponent()} autoPlay controls /> 
                     :
                     null
                     }
