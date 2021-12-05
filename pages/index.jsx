34e1a3dea1285918a559a419a62b1d128529a43c
Borislava <bobiivanova00@gmail.com>
2021-12-05 18:03:21 +0200

    add onClick media functionallity

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import Header from "../src/components/header/Header";
 import Navigation from "../src/components/navigation/Navigation";
 import Thermostat from "../src/components/thermostat/Thermostat";
 import Scenes from "../src/components/scenes/Scenes";
+import Cameras from "../src/components/cameras/Cameras";
 
 export default function Index() {
   const data = [
@@ -15,16 +16,66 @@ export default function Index() {
     { temperature: 15, hour: 16 },
     { temperature: 16, hour: 17 },
   ]
+
+  {/*const cameras = {
+    "cameras": [
+      {
+        "videoUrl": "../videos/balcony.mp4"
+      },
+      {
+        "videoUrl": "../videos/bathroom.mp4"
+      },
+      {
+        "videoUrl": "../videos/front-door.mp4"
+      },
+      {
+        "videoUrl": "../videos/garden.mp4"
+      },
+      {
+        "videoUrl": "../videos/kitchen.mp4"
+      },
+      {
+        "videoUrl": "../videos/living-room-2.mp4"
+      },
+    ],
+    "hasButton": Boolean
+  }*/}
+
+  const cameras = {
+    "cameras": [
+      {
+        "videoUrl": "../images/sunny.svg"
+      },
+      {
+        "videoUrl": "../images/avatar.png"
+      },
+      {
+        "videoUrl": "../images/cloudy.svg"
+      },
+      {
+        "videoUrl": "../images/morning.svg"
+      },
+      {
+        "videoUrl": "../images/shower.svg"
+      },
+      {
+        "videoUrl": "../images/snowy.svg"
+      },
+    ],
+    "hasButton": Boolean
+  }
+
   return (
     <>
       <Header left={<User avatar="../images/avatar.png" name="Jonh Doe" headingSize="h3" />} right={<><Weather degrees={22} type="cloudy" /> <Time /></>} />
       <Navigation />
-      <Thermostat data={data} />
+     {/* <Thermostat data={data} /> */}
       <Scenes cards={[
         { iconUrl: '../images/alarm-clock.svg', outlined: false },
         { iconUrl: '../images/alarm-clock.svg', outlined: false },
         { iconUrl: '../images/alarm-clock.svg', outlined: false }
       ]} />
+      <Cameras {...cameras} />
     </>
   )
 }
