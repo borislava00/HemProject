af7cf76e4025e5bf8cee389142330d3653f634d7
Borislava <bobiivanova00@gmail.com>
2021-12-09 20:00:21 +0200

    change onSumbit button

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,29 +1,25 @@
 import { Dialog , DialogTitle , DialogContent , Button  , DialogContentText , DialogActions }  from "@mui/material";
-import { useState } from "react";
 
 import styles from "./Modal.module.scss";
 
 export default function Modal({ open , title , buttonText , handleSubmit , handleClose }) {
-    const [isOpen, setIsOpen] = useState(open);
-  
-
     return(
         <div className={styles['modal-wrapper']}>
-             <Button variant="outlined" onClick={() => setIsOpen(true)}>
+             <Button variant="outlined" onClick={handleSubmit}>
                 {buttonText}
             </Button>
             <Dialog
-                open={isOpen}
+                open={open}
                 onClose={handleClose}
             >
             <DialogTitle className={styles.title}>{title}</DialogTitle>
             <DialogContent>
                 <DialogContentText className={styles.content}>
-                    Modal Content
+                    {title}
                 </DialogContentText>
             </DialogContent>
             <DialogActions>
-                <Button onClick={() => setIsOpen(false)}>Modal Action</Button>
+                <Button onClick={handleClose}>Modal Action</Button>
             </DialogActions>
             </Dialog>
         </div>
