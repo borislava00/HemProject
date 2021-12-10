9294f9a4d2d14104b725ff45cba3190bebea941e
Borislava <bobiivanova00@gmail.com>
2021-12-10 10:40:19 +0200

    add ChildExample component

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,27 +1,42 @@
-import { Dialog , DialogTitle , DialogContent , Button  , DialogContentText , DialogActions }  from "@mui/material";
+import { Dialog , DialogTitle , DialogContent , Button , DialogActions }  from "@mui/material";
+import { useState } from "react";
 
 import styles from "./Modal.module.scss";
 
 export default function Modal({ open , title , buttonText , handleSubmit , handleClose }) {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const handleClickOpen = () => {
+      setIsOpen(true);
+    };
+  
+    const handleOnClose = () => {
+      setIsOpen(false);
+    };
     return(
         <div className={styles['modal-wrapper']}>
-             <Button variant="outlined" onClick={handleSubmit}>
+             <Button variant="outlined" onClick={handleClickOpen}>
                 {buttonText}
             </Button>
             <Dialog
-                open={open}
+                open={isOpen}
                 onClose={handleClose}
             >
             <DialogTitle className={styles.title}>{title}</DialogTitle>
-            <DialogContent>
-                <DialogContentText className={styles.content}>
-                    {title}
-                </DialogContentText>
+            <DialogContent className={styles.content} >
+                <ChildExample />
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose}>Modal Action</Button>
+                <Button onClick={handleSubmit}>Modal Action</Button>
+                <Button onClick={handleOnClose}>Close</Button>
             </DialogActions>
             </Dialog>
         </div>
+    );
+}
+
+function ChildExample() {
+    return(
+        <div>Modal Content</div>
     );
 }
