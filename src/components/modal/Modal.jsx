4e8dc8b91c73bb8ab2d7d04ed74fd7914c4617d3
Borislava <bobiivanova00@gmail.com>
2021-12-10 10:52:41 +0200

    add DialogContent

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,35 +1,21 @@
 import { Dialog , DialogTitle , DialogContent , Button , DialogActions }  from "@mui/material";
-import { useState } from "react";
 
 import styles from "./Modal.module.scss";
 
 export default function Modal({ open , title , buttonText , handleSubmit , handleClose }) {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const handleClickOpen = () => {
-      setIsOpen(true);
-    };
-  
-    const handleOnClose = () => {
-      setIsOpen(false);
-    };
     return(
         <div className={styles['modal-wrapper']}>
-             <Button variant="outlined" onClick={handleClickOpen}>
-                {buttonText}
-            </Button>
             <Dialog
-                open={isOpen}
+                open={open}
                 onClose={handleClose}
             >
-            <DialogTitle className={styles.title}>{title}</DialogTitle>
-            <DialogContent className={styles.content} >
-                <ChildExample />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={handleSubmit}>Modal Action</Button>
-                <Button onClick={handleOnClose}>Close</Button>
-            </DialogActions>
+                <DialogTitle className={styles.title}>{title}</DialogTitle>
+                <DialogContent className={styles.content} >
+                    <ChildExample />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleSubmit}>{buttonText}</Button>
+                </DialogActions>
             </Dialog>
         </div>
     );
