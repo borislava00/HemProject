c1f5797ad26025d29d2c78e7f1b85776a22b47b5
kokiqn <kaloyna.stefanoff@gmail.com>
2021-12-30 13:28:12 +0200

    feat: implement login API calls

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,27 +3,52 @@ import styles from './Login.module.scss';
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
 
-export default function Login() {
+export default function Login({ setEmail, setPassword, onSubmit }) {
+    function handleEmailChange(e) {
+        setEmail(e.target.value);
+    }
+
+    function handlePasswordChange(e) {
+        setPassword(e.target.value);
+    }
+
     return (
         <div className={styles["login-form"]}>
             <Grid container rowSpacing={1} justifyContent="center">
                 <Grid item xs={10}>
-                    <TextField fullWidth className={styles.input}
+                    <TextField
+                        fullWidth
+                        className={styles.input}
+                        type="email"
+                        onChange={handleEmailChange}
                         InputProps={{
-                            startAdornment: <InputAdornment position="start" sx={{ color: 'black' }}><EmailIcon /></InputAdornment>,
-                        }}>
-
-                    </TextField>
+                            startAdornment:
+                                <InputAdornment
+                                    position="start"
+                                    sx={{ color: 'black' }}><EmailIcon />
+                                </InputAdornment>,
+                        }} />
                 </Grid>
                 <Grid item xs={10}>
-                    <TextField fullWidth className={styles.input}
+                    <TextField
+                        fullWidth
+                        className={styles.input}
+                        type="password"
+                        onChange={handlePasswordChange}
                         InputProps={{
-                            startAdornment: <InputAdornment position="start" sx={{ color: 'black' }}><LockIcon /></InputAdornment>,
-                        }}>
-                    </TextField>
+                            startAdornment:
+                                <InputAdornment
+                                    position="start"
+                                    sx={{ color: 'black' }}><LockIcon />
+                                </InputAdornment>,
+                        }} />
                 </Grid>
                 <Grid item xs={10}>
-                    <Button fullWidth variant="contained" className={styles.button}>
+                    <Button
+                        fullWidth
+                        variant="contained"
+                        className={styles.button}
+                        onClick={onSubmit}>
                         Login
                     </Button>
                 </Grid>
