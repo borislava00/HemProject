8d036a6dcd9544069ee06ce746f06cd71d02d963
jivko25 <jivkou@yahoo.com>
2022-01-10 19:18:43 +0200

    Add solution

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,13 +1,15 @@
 import styles from './LoginPage.module.scss';
 import { Container, Paper } from '@mui/material';
 import Login from '../../src/components/login/Login';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { useRouter } from 'next/dist/client/router';
+import { AppContext } from '../../src/components/common/AppProvider';
 
 export default function LoginPage() {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [refresh] = useContext(AppContext);
 
   async function loginUser() {
 
@@ -27,6 +29,7 @@ export default function LoginPage() {
         router.push('/');
       } else {
         alert(`The email ${email} is not registered!`);
+        refresh()
         router.push('/register');
       }
   }
