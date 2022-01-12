23f61dd3d8e2a356ad34c46b1cec36cb6dbac63e
kokiqn <kaloyan.stefanoff@gmail.com>
2022-01-12 18:24:29 +0200

    style: Login component design

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,58 +1,72 @@
-import { Button, Grid, InputAdornment, TextField } from '@mui/material';
-import styles from './Login.module.scss';
-import EmailIcon from '@mui/icons-material/Email';
-import LockIcon from '@mui/icons-material/Lock';
+import { Button, Grid, InputAdornment, TextField } from "@mui/material";
+import styles from "./Login.module.scss";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  noBorder: {
+    border: "none",
+    borderRadius: 0,
+  },
+  input: {
+    background: "#f8f8f8",
+    padding: 8,
+  },
+});
 
 export default function Login({ setEmail, setPassword, onSubmit }) {
-    function handleEmailChange(e) {
-        setEmail(e.target.value);
-    }
+  const classes = useStyles();
 
-    function handlePasswordChange(e) {
-        setPassword(e.target.value);
-    }
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
 
-    return (
-        <div className={styles["login-form"]}>
-            <Grid container rowSpacing={1} justifyContent="center">
-                <Grid item xs={10}>
-                    <TextField
-                        fullWidth
-                        className={styles.input}
-                        type="email"
-                        onChange={handleEmailChange}
-                        InputProps={{
-                            startAdornment:
-                                <InputAdornment
-                                    position="start"
-                                    sx={{ color: 'black' }}><EmailIcon />
-                                </InputAdornment>,
-                        }} />
-                </Grid>
-                <Grid item xs={10}>
-                    <TextField
-                        fullWidth
-                        className={styles.input}
-                        type="password"
-                        onChange={handlePasswordChange}
-                        InputProps={{
-                            startAdornment:
-                                <InputAdornment
-                                    position="start"
-                                    sx={{ color: 'black' }}><LockIcon />
-                                </InputAdornment>,
-                        }} />
-                </Grid>
-                <Grid item xs={10}>
-                    <Button
-                        fullWidth
-                        variant="contained"
-                        className={styles.button}
-                        onClick={onSubmit}>
-                        Login
-                    </Button>
-                </Grid>
-            </Grid>
-        </div>
-    );
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  return (
+    <div className={styles["login-form"]}>
+      <Grid container rowSpacing={1} justifyContent="center">
+        <Grid item xs={10}>
+          <TextField
+            className={classes.input}
+            fullWidth
+            type="email"
+            onChange={handleEmailChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start" sx={{ color: "black" }}>
+                  <EmailIcon />
+                </InputAdornment>
+              ),
+              classes: { notchedOutline: classes.noBorder },
+            }}
+          />
+        </Grid>
+        <Grid item xs={10}>
+          <TextField
+            className={classes.input}
+            fullWidth
+            type="password"
+            onChange={handlePasswordChange}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start" sx={{ color: "black" }}>
+                  <LockIcon />
+                </InputAdornment>
+              ),
+              classes: { notchedOutline: classes.noBorder },
+            }}
+          />
+        </Grid>
+        <Grid item xs={10}>
+          <Button fullWidth variant="contained" className={styles.button} onClick={onSubmit}>
+            Login
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
