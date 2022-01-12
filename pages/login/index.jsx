27ecead5fde7b00233b789b1eff0b9e4fa0deb75
kokiqn <kaloyan.stefanoff@gmail.com>
2022-01-12 20:32:38 +0200

    feat: add redirect to register page

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,51 +1,50 @@
-import styles from './LoginPage.module.scss';
-import { Container, Paper } from '@mui/material';
-import Login from '../../src/components/login/Login';
-import { useContext, useState } from 'react';
-import { useRouter } from 'next/dist/client/router';
-import { AppContext } from '../../src/components/common/AppProvider';
+import styles from "./LoginPage.module.scss";
+import { Container, Paper, Typography } from "@mui/material";
+import Login from "../../src/components/login/Login";
+import { useContext, useState } from "react";
+import { useRouter } from "next/dist/client/router";
+import { AppContext } from "../../src/components/common/AppProvider";
+import Link from "../../src/components/link/Link";
 
 export default function LoginPage() {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [refresh] = useContext(AppContext);
 
   async function loginUser() {
-
-    const response = await fetch('https://hem-api.herokuapp.com/login', {
-      method: 'POST',
+    const response = await fetch("https://hem-api.herokuapp.com/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        'email': email,
-        'password': password,
+        email: email,
+        password: password,
       }),
-    })
-      if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem('accessToken', data['accessToken']);
-        refresh()
-        router.push('/');
-        refresh()
-      } else {
-        alert(`The email ${email} is not registered!`);
-        router.push('/register');
-      }
+    });
+    if (response.ok) {
+      const data = await response.json();
+      localStorage.setItem("accessToken", data["accessToken"]);
+      refresh();
+      router.push("/");
+      refresh();
+    } else {
+      alert(`The email ${email} is not registered!`);
+      router.push("/register");
+    }
   }
 
   return (
     <div className={styles.wrapper}>
-      <Container maxWidth='xs'>
+      <Container maxWidth="xs">
         <Paper elevation={24}>
-          <Login
-            setEmail={setEmail}
-            setPassword={setPassword}
-            onSubmit={loginUser}
-          />
+          <Login setEmail={setEmail} setPassword={setPassword} onSubmit={loginUser} />
         </Paper>
+        <Typography fontSize={15} className={styles.text}>
+          Don't have an account? <Link href="/register">Register today!</Link>
+        </Typography>
       </Container>
     </div>
-  )
+  );
 }
